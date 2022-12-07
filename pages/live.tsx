1a2a7e5bb3e55cf15531ed25d3a99011d17cb099
Gabe Krahulik <gabekrahulik@gmail.com>
2022-12-07 09:11:32 -0800

    add Pit Wall button to the live page

--- pages/live.tsx
+++ pages/live.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Loading, SEO } from '../components';
+import { Loading, SEO, Button } from '../components';
 
 import { AiFillEye } from 'react-icons/ai';
 
@@ -31,55 +31,29 @@ export default function Home() {
 	})
 
     const [channels, setChannels] = useState([
-        {
-          "id": "71778837",
-          "name": "skiggity242"
-      },
-      {
-          "id": "7443503",
-          "name": "pennyarcade"
-      },
-      {
-        "name": "mreggman__"
-      },
-      {
-        "name": "cluus_"
-      },
-      {
-        "name": "keyma5ter"
-      },
-      {
-        "name": "iracing"
-      },
-      {
-        "name": "wardingo37"
-      },
-      {
-        "name": "mattcribbsracing"
-      },
-      {
-        "name": "biorebek"
-      },
-      {
-        "name": "hoekeheef"
-      },
-      {
-        "name": "draxond"
-      },
-      {
-        "name": "ravenholm337"
-      },
-      {
-        "name": "red_stapler"
-      },
-      {
-        "name": "beastgp"
-      },
-      {
-        "name": "oneletter"
-      }
+      "skiggity242",
+      "pennyarcade",
+      "mreggman__",
+      "cluus_",
+      "keyma5ter",
+      "iracing",
+      "wardingo37",
+      "mattcribbsracing",
+      "biorebek",
+      "hoekeheef",
+      "draxond",
+      "ravenholm337",
+      "red_stapler",
+      "beastgp",
+      "oneletter",
+      "ducksoup",
+      "veeveevonvore",
+      "swettpants",
+      "gargosthemighty"
     ])
     const [channelData, setChannelData] = useState([]);
+    const [iracingChannelData, setIracingChannelData] = useState([]);
+    const [pitwallChannels, setPitwallChannels] = useState([]);
 
     useEffect(() => {
 		document.getElementById("bg").style.backgroundImage = `url(${theme.backgroundImage})`;
@@ -88,9 +62,14 @@ export default function Home() {
 
     useEffect(() => {
         (async () => {
+            const p_res = await fetch("https://streaming.gabirmotors.com/pitwall/channels");
+            const p_data = await p_res.json();
+
+            setPitwallChannels(p_data.data || []);
+
             let userIds = "";
             for (let i = 0; i < channels.length; i ++) {
-                userIds += "user_login=" + channels[i].name + "&";
+                userIds += "user_login=" + channels[i] + "&";
             }
             
             const res = await fetch("https://api.twitch.tv/helix/streams?" + userIds, {
@@ -100,45 +79,61 @@ export default function Home() {
                 }
             });
             const data = await res.json();
-            // console.log(data)
-            setChannelData(data.data);
+
+            // game_name = "iracing";
+            let _tempData = [ ...data.data ];
+            let _tempIracing = [];
+
+            for (let i = 0; i < _tempData.length; i ++) {
+              if (_tempData[i].game_name.toLowerCase() === "iracing") {
+                _tempIracing.push(_tempData[i]);
+              }
+            }
+
+            setChannelData(_tempData);
+            setIracingChannelData(_tempIracing);
+            console.log(channelData);
         })()
     }, [ channels ])
     
 	return (
 		<div>
 			<SEO
-                title="Pit Wall TV"
-                url="live"
-            />
+        title="Pit Wall TV"
+        url="live"
+      />
 
 			<Loading loading = { loading } />
 
 			<div id = "bg" className = {`${theme.theme === "dark" ? "dark" : ""} background min-h-screen h-auto`}>
 				<div className = "text-black dark:text-white flex flex-col justify-center">
-                    <h1 className = "acumin font-bold text-5xl block text-center">Pit Wall TV</h1>
-                    <div className = "grid grid-cols-1 lg:grid-cols-4">
-                        {/* <pre>{JSON.stringify(channelData, null, 4)}</pre> */}
-                        { channelData.map(channel => {
-                            return (
-                                <a href={"https://twitch.tv/" + channel.user_name} target = "_new">
-                                    <div className = "m-4 bg-dark-card-body p-6 rounded-md flex flex-col hover:bg-dark-card-handle transition duration-200">
-                                        <img src={"https://static-cdn.jtvnw.net/previews-ttv/live_user_" + channel.user_login + "-1920x1080.jpg"} alt="Stream Preview" />
+            <h1 className = "acumin font-bold text-5xl block text-center">Pit Wall TV</h1>
+              <div className = "grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+                {/* <pre>{JSON.stringify(pitwallChannels, null , 4)}</pre> */}
+                { channelData.map(channel => {
+                    return (
+                        <a href={"https://twitch.tv/" + channel.user_name} target = "_new">
+                            <div className = "m-4 bg-dark-card-body p-6 rounded-md flex flex-col hover:bg-dark-card-handle transition duration-200">
+                                <img src={"https://static-cdn.jtvnw.net/previews-ttv/live_user_" + channel.user_login + "-1920x1080.jpg"} alt="Stream Preview" />
 
-                                        <span className = "text-left font-bold p-2">{ channel.title }</span>
+                                <span className = "text-left font-bold p-2">{ channel.title }</span>
 
-                                        <div className="flex flex-row gap-2 justify-between">
-                                            <span>{ channel.user_name }</span>
-                                            <span className = "opacity-70 italic">{ timeSince(new Date(channel.started_at)) }</span>
-                                            <span className = "text-red-500"><AiFillEye className = "inline-block"/>{ channel.viewer_count }</span>
-                                        </div>
-                                        {/* <pre>{JSON.stringify(channel, null, 4)}</pre> */}
-                                    </div>
-                                </a>
-                            )
-                        }) }
-                    </div>
-                </div>
+                                <div className="flex flex-row gap-2 justify-between">
+                                    <span>{ channel.user_name }</span>
+                                    <span className = "opacity-70 italic">{ timeSince(new Date(channel.started_at)) }</span>
+                                    <span className = "text-red-500"><AiFillEye className = "inline-block"/>{ channel.viewer_count }</span>
+                                </div>
+
+                                { pitwallChannels.some(p_channel => { return p_channel === channel.user_name }) ? (
+                                  <Button block link = {`/user/${channel.user_name}`}>Visit Pit Wall</Button>
+                                ) : "" }
+                                {/* <pre>{JSON.stringify(channel, null, 4)}</pre> */}
+                            </div>
+                        </a>
+                    )
+                }) }
+            </div>
+        </div>
 			</div>
 		</div>
 	)
